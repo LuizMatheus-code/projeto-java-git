69bcaef5315cfe9f21f540577ff9abceee7657e6
Luiz Matheus <nomenenhum190@gmail.com>
2025-02-08 11:18:27 -0300

    Notas

--- projeto-java-git/src/App.java
+++ projeto-java-git/src/App.java
@@ -7,6 +7,12 @@ public class App {
         System.out.println("Digite seu nome");
         String nome = entrada.nextLine();
 
+        System.out.println("Digite a primeira nota");
+        double nota1 = entrada.nextDouble();
+
+        System.out.println("Digite a segunda nota");
+        double nota2 = entrada.nextDouble();
+
         entrada.close();
 
         System.out.println("Olá, " + nome + "!");
