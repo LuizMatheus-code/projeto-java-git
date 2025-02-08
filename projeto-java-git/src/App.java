13be2a10705416a8cdb8b676ad3ca63a407ffd6b
Luiz Matheus <nomenenhum190@gmail.com>
2025-02-08 11:28:16 -0300

    Media

--- projeto-java-git/src/App.java
+++ projeto-java-git/src/App.java
@@ -2,19 +2,22 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite seu nome");
-        String nome = entrada.nextLine();
+        String nome = sc.nextLine();
 
         System.out.println("Digite a primeira nota");
-        double nota1 = entrada.nextDouble();
+        double nota1 = sc.nextDouble();
 
         System.out.println("Digite a segunda nota");
-        double nota2 = entrada.nextDouble();
+        double nota2 = sc.nextDouble();
 
-        entrada.close();
+        double media = (nota1 + nota2) / 2;
+
+        sc.close();
 
         System.out.println("Olá, " + nome + "!");
+        System.out.println("As médias das notas " + nota1 + " e " + nota2 + " fica " + media);
     }
 }
