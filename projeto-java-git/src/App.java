2d8210ac4259725b8cee649e2ab42af5bbaf3303
Luiz Matheus <nomenenhum190@gmail.com>
2025-02-08 11:15:40 -0300

    Entrada e saída de dados

--- projeto-java-git/src/App.java
+++ projeto-java-git/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite seu nome");
+        String nome = entrada.nextLine();
+
+        entrada.close();
+
+        System.out.println("Olá, " + nome + "!");
     }
 }
